7f3399d53e9e32c1565fdaa3d9338a5dc421b977
Sai <saikrishnasmarty@gmail.com>
2024-04-15 23:39:45 +0530

    adding profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
+      <Profile />
       <header className="App-header">I'm hero !</header>
     </div>
   );
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+function Profile() {
+    return (
+      <div className="Profile">
+        <header className="Profile-header">This is profile header</header>
+      </div>
+    );
+  }
+  
+  export default Profile;
